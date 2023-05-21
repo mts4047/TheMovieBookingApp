98fcf7e85d150c96d8c0cc2405780886caa5972b
MTS <40478665@live.napier.ac.uk>
2023-05-21 23:01:05 +0630

    first commit

---
+++ TheMovieBookingAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TheMovieBookingAppApp.swift
+//  TheMovieBookingApp
+//
+//  Created by Myo Thandar soe on 21/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct TheMovieBookingAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
